35e9ccb25e6a54c07f2cb90395071c718bd7170f
rodrigol-oliveira <rodrigo.luiz@msn.com>
2016-05-26 18:20:06 -0300

    integração das telas feitas com o BD

--- app.js
+++ app.js
@@ -1,77 +1,63 @@
-var express = require('express');
-var ejs = require('ejs');
-var mysql = require('mysql');
-var bodyParser = require('body-parser');
-var path = require('path');
+	var express = require('express');
+	var ejs = require('ejs');
+	var mysql = require('mysql');
+	var bodyParser = require('body-parser');
+	var path = require('path');
 
 
-var app = express();
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.set('views', __dirname + '/views');
-app.use(express.static('public'));
-app.locals.pretty = true;
-app.use(express.static(__dirname + '/views'));
+	var app = express();
+	app.set('view engine', 'ejs');
+	app.use(bodyParser.urlencoded({ extended: false }))
+	app.use(bodyParser.json())
+	app.set('views', __dirname + '/views');
+	app.use(express.static('public'));
+	app.locals.pretty = true;
+	app.use(express.static(__dirname + '/views'));
 
-
-var connection = mysql.createConnection({
-	host		: 'localhost',
-	user		: 'root',
-	password	: '',
-	database	: 'Arduino'
-});
-app.post('/adduser',function(req, res){
-	var name = req.body.name;
-		//var email = req.params.email;
-		//var password = req.params.password;
-		//var city_id = req.params.city_id;
-		//connection.query('INSERT INTO user ( name,email,password,city_id) VALUES (?,?,?,?)', 
-		//[ name,email,password,city_id ] , function(err, res){
-			connection.query('INSERT INTO user (name) VALUES (?)', [name] , function(err, res){
+//Metodo de conexão
+	var connection = mysql.createConnection({
+		host		: 'localhost',
+		user		: 'root',
+		password	: '',
+		database	: 'Arduino'
+	});
+//metodo de adicionar usuario no BD
+	app.post('/adduser',function(req, res){
+		var name = req.body.name;
+		var email = req.body.email;
+		var password = req.body.password;
+		connection.query('INSERT INTO user (name,email,password) VALUES (?,?,?)', [ name, email, password ] , function(err, res){
 				if(err) throw err;
 			});
-			res.render("ok");
+		res.render('index', {message: 'Cadastro OK'});
 		});
 
-app.post('/entrar',function(req, res){
-	var name = req.body.name;
-		//var email = req.params.email;
-		//var password = req.params.password;
-		//var city_id = req.params.city_id;
-		//connection.query('INSERT INTO user ( name,email,password,city_id) VALUES (?,?,?,?)', 
-		//[ name,email,password,city_id ] , function(err, res){
-		connection.query('INSERT INTO user (name) VALUES (?)', [name] , function(err, res){
-				if(err) throw err;
-		});
-			res.render("ok");
-		});
+//Pagina requisita pagina HOME
+	app.get('/',function(req,res){
+		res.render('index', {message: ''});
+	});
 
-app.get('/',function(req,res){
-	res.render('index', {message: 'variavel vinda do node.js'})
-	//res.sendFile('index.html');
-	//res.render('user_add')
-  //It will find and locate index.html from View or Scripts
-});
-app.get('/cadastro',function(req,res){
-	res.render('cadastro')
-});
+//Metodo requisita pagina de cadastro
+	app.get('/cadastro',function(req,res){
+		res.render('cadastro');
+	});
 
-app.get('/login', function(req, res){
-		//res.send('<pre>Arduino</pre>');
+//metodo requisita pagina de Login
+	app.get('/login', function(req, res){
 		res.render('login');
 	});
 
-app.get('/main', function(req, res){
+//Metodo requisita pagina Main
+	app.get('/main', function(req, res){
 		//res.send('<pre>Arduino</pre>');
 		res.render('main');
 	});
 
-
-connection.connect(function(err){
-	if(err) throw err;
-	console.log('Conectado no MySQL'); 	
-	app.listen(3000, function(){
+//Metodo de Conexão
+	connection.connect(function(err){
+		if(err) throw err;
+		console.log('Conectado no MySQL'); 	
+		app.listen(3000, function(){
 		console.log('Servidor Arduino -> http://localhost:3000');
+		});
 	});
-});
